426aec8fc29bfbe87898eb241d6d779702dddaad
manueldelapenna <manueldelapenna@gmail.com>
2024-01-15 16:30:05 -0300

    historial tem

--- operaciones/2024-01-12-actualizar_tareas_proximas.sql
+++ operaciones/2024-01-12-actualizar_tareas_proximas.sql
@@ -99,3 +99,79 @@ CREATE TRIGGER desverificar_tarea_trg
 
 DROP TRIGGER IF EXISTS determinar_tarea_proxima_trg ON tareas_tem;
 DROP FUNCTION if exists determinar_tarea_proxima_trg();
+
+create table "historial_tem" (
+  "operativo" text, 
+  "enc" text, 
+  "orden" integer, 
+  "ts_salida" timestamp, 
+  "tarea" text, 
+  "recepcionista" text, 
+  "asignado" text, 
+  "json_encuesta" jsonb, 
+  "resumen_estado" text, 
+  "resumen_estado_sup" text, 
+  "rea" integer, 
+  "norea" integer, 
+  "rea_sup" integer, 
+  "norea_sup" integer
+, primary key ("operativo", "enc", "orden")
+);
+grant select, insert, update on "historial_tem" to eah_admin;
+grant all on "historial_tem" to eah_owner;
+
+alter table "historial_tem" add constraint "operativo<>''" check ("operativo"<>'');
+alter table "historial_tem" add constraint "enc<>''" check ("enc"<>'');
+alter table "historial_tem" add constraint "tarea<>''" check ("tarea"<>'');
+alter table "historial_tem" add constraint "recepcionista<>''" check ("recepcionista"<>'');
+alter table "historial_tem" add constraint "asignado<>''" check ("asignado"<>'');
+alter table "historial_tem" add constraint "resumen_estado<>''" check ("resumen_estado"<>'');
+alter table "historial_tem" add constraint "resumen_estado_sup<>''" check ("resumen_estado_sup"<>'');
+
+alter table "historial_tem" add constraint "historial_tem tem REL" foreign key ("operativo", "enc") references "tem" ("operativo", "enc")  on update cascade;
+alter table "historial_tem" add constraint "historial_tem tareas REL" foreign key ("operativo", "tarea") references "tareas" ("operativo", "tarea")  on update cascade;
+alter table "historial_tem" add constraint "historial_tem asignado REL" foreign key ("asignado") references "usuarios" ("idper")  on update cascade;
+alter table "historial_tem" add constraint "historial_tem recepcionista REL" foreign key ("recepcionista") references "usuarios" ("idper")  on update cascade;
+
+create index "operativo,enc 4 historial_tem IDX" ON "historial_tem" ("operativo", "enc");
+create index "operativo,tarea 4 historial_tem IDX" ON "historial_tem" ("operativo", "tarea");
+create index "asignado 4 historial_tem IDX" ON "historial_tem" ("asignado");
+create index "recepcionista 4 historial_tem IDX" ON "historial_tem" ("recepcionista");
+
+CREATE OR REPLACE FUNCTION agregar_historial_tem_trg()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+AS $BODY$
+    declare
+        v_proximo_orden integer;
+        v_recepcionista text;
+        v_asignado text;
+        v_json_encuesta jsonb;
+        v_resumen_estado text;
+        v_resumen_estado_sup text;
+        v_rea integer;
+        v_norea integer;
+        v_rea_sup integer;
+        v_norea_sup integer;
+begin
+    if old.tarea_actual is distinct from new.tarea_actual and old.tarea_actual is not null then
+        select coalesce(max(orden),0)+1 into v_proximo_orden
+            from historial_tem 
+            where operativo = old.operativo and enc = old.enc;
+        
+        select recepcionista, asignado, resumen_estado, resumen_estado_sup, rea, norea, rea_sup, norea_sup  into
+            v_recepcionista, v_asignado, v_resumen_estado, v_resumen_estado_sup, v_rea, v_norea, v_rea_sup, v_norea_sup 
+            from tareas_tem 
+            where operativo = old.operativo and enc = old.enc and tarea = old.tarea_actual;
+        
+        select json_encuesta  into
+            v_json_encuesta
+            from tem 
+            where operativo = old.operativo and enc = old.enc;
+
+        insert into historial_tem (operativo, enc, orden, tarea, ts_salida, recepcionista, asignado, json_encuesta, resumen_estado, resumen_estado_sup, rea, norea, rea_sup, norea_sup) values 
+            (old.operativo, old.enc, v_proximo_orden, old.tarea_actual, current_timestamp, v_recepcionista, v_asignado, v_json_encuesta, v_resumen_estado, v_resumen_estado_sup, v_rea, v_norea, v_rea_sup, v_norea_sup);
+    end if;
+    return new;
+end;
+$BODY$;
