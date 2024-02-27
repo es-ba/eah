5d569e371d06239c5d3464d1b4f5b8caaa7554b5
manueldelapenna <manueldelapenna@gmail.com>
2024-02-27 18:38:28 -0300

    actualizo dependencias

--- operaciones/2024-01-12-actualizar_tareas_proximas.sql
+++ operaciones/2024-01-12-actualizar_tareas_proximas.sql
@@ -83,9 +83,11 @@ AS $BODY$
 declare 
         v_asignado_actual text;
         v_tarea_actual text;
+        v_verificado_actual text;
         v_ultima_tarea_historial text;
+        v_tarea_final text;
 begin
-    select asignado, tarea_actual into v_asignado_actual, v_tarea_actual
+    select asignado, tarea_actual, verificado into v_asignado_actual, v_tarea_actual, v_verificado_actual
         from tareas_tem tt join tem t using (operativo, enc)
         where new.operativo = t.operativo and new.enc = t.enc and tt.tarea = t.tarea_actual;
 
@@ -94,8 +96,14 @@ begin
         where new.operativo = ht.operativo and new.enc = ht.enc
         order by ts_salida desc
         limit 1;
+    select tarea into v_tarea_final
+        from tareas 
+        where operativo = new.operativo and es_final;
     if new.verificado is null then
         if v_ultima_tarea_historial = new.tarea then 
+            if v_verificado_actual is not null then
+                raise 'ERROR: no se puede desverificar la tarea % ya que la tarea siguiente (%) se encuentra verificada', new.tarea, v_tarea_actual;
+            end if;
             if v_asignado_actual is null then
                 update tem 
                     set tarea_actual = new.tarea
@@ -104,7 +112,9 @@ begin
                 raise 'ERROR: no se puede desverificar la tarea % ya que la tarea siguiente (%) se encuentra asignada', new.tarea, v_tarea_actual;
             end if;
         else
-            raise 'ERROR: no se puede desverificar la tarea % ya que no es la ultima anterior a la actual', new.tarea;
+            if v_tarea_final is distinct from new.tarea then
+                raise 'ERROR: no se puede desverificar la tarea % ya que no es la ultima anterior a la actual', new.tarea;
+            end if;
         end if;
     end if;
     return new;
@@ -197,7 +207,8 @@ CREATE TRIGGER agregar_historial_tem_trg
    FOR EACH ROW
    EXECUTE PROCEDURE agregar_historial_tem_trg();   
 
-alter table "tareas" add column "es_inicial";
+alter table "tareas" add column "es_inicial" boolean;
+alter table "tareas" add column "es_final" boolean;
 
 update tareas set es_inicial = true where operativo = 'PRC_EAH2023' and tarea = 'encu';
 
